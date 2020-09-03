73d1296782de791e881749c7d48b96d7846195b0
Deri Herdianto <derryherdianto@gmail.com>
2020-09-04 06:40:56 +0700

    Ubah nama jadi dashboard

--- router/admin.dashboard.go
+++ router/admin.dashboard.go
@@ -10,9 +10,10 @@ import (
 // Halaman admin
 func PageAdmin(c *gin.Context) {
 	gh := gin.H{
-		"site_title": "Halaman Admin",
+		"site_title": "Halaman Dashboard Admin",
+		"page":       "dashboard",
 	}
 
 	df := c.MustGet("config").(misc.DefaultConfig).Site
-	c.HTML(200, "admin.html", misc.Mete(df, gh))
+	c.HTML(200, "admin.dashboard.html", misc.Mete(df, gh))
 }
