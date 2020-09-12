060a9f8671090adc99ab667d76f6accd2f8df559
Deri Herdianto <derryherdianto@gmail.com>
2020-09-12 08:26:20 +0700

    Test

--- test.go
+++ test.go
@@ -1,26 +1,49 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"os"
+
+	"github.com/gocarina/gocsv"
 )
+
+type Data struct {
+	Code      string  `csv:"Code"`
+	Parent    int     `csv:"Parent"`
+	Name      string  `csv:"Name"`
+	Latitude  float32 `csv:"Latitude"`
+	Longitude float32 `csv:"Longitude"`
+	//Postal    string  `csv:"Postal"`
+}
 
 // Buat ngetest csv hhh
 func main() {
-	openCountries()
+	openData("provinces.csv")
 }
 
-func openCountries() {
-	file, err := os.Open("./setup/sub-districts.csv")
+func openData(d string) {
+	file, err := os.Open(fmt.Sprintf("./setup/%s", d))
+	//file, err := os.OpenFile("./setup/villages.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
 
-	read := csv.NewReader(file)
-	records, err := read.ReadAll()
+	data := []*Data{}
 
-	fmt.Println("Rekor", records)
+	if err := gocsv.UnmarshalFile(file, &data); err != nil {
+		fmt.Println("Telah terjadi error")
+		fmt.Println(err)
+	}
+
+	for no, da := range data {
+		fmt.Println("Nomor Urut", no)
+		fmt.Println("Kode", da.Code)
+		fmt.Println("Turunan", da.Parent)
+		fmt.Println("Nama", da.Name)
+		fmt.Println("Latitude", da.Latitude)
+		fmt.Println("Longitude", da.Longitude)
+		fmt.Println("Kode Pos", da.Postal)
+	}
 
 }
