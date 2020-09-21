20b33d43603fa1368900f7ddf1782c15ea2517bf
Deri Herdianto <derryherdianto@gmail.com>
2020-09-22 05:36:29 +0700

    Mengganti ` ke "

--- setup/tables.sql
+++ setup/tables.sql
@@ -4,9 +4,10 @@
 *  c 2020
 */
 
--- name: create-role-table
-CREATE TABLE `role` (
-    `id` SMALLINT GENERATED ALWAYS AS IDENTITY,
-    `name` VARCHAR(10),
+DROP TABLE IF EXISTS "role";
+
+CREATE TABLE "role" (
+    "id" SMALLINT GENERATED ALWAYS AS IDENTITY,
+    "name" VARCHAR(10),
     PRIMARY KEY (id)
 );
