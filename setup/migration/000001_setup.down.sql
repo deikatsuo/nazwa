42908606aa0513c5c7c448e5f4b788cc414e7408
Deri Herdianto <derryherdianto@gmail.com>
2020-09-27 16:31:54 +0700

    Menggunakan database migration

---
+++ setup/migration/000001_setup.down.sql
@@ -0,0 +1,13 @@
+/* Nazwa
+*  Membuat tabel utama
+*  Authored by Deri Herdianto
+*  c 2020
+*/
+
+-- DROP semua
+DROP TABLE IF EXISTS "user_role";
+DROP TABLE IF EXISTS "role";
+DROP TABLE IF EXISTS "email";
+DROP TABLE IF EXISTS "phone";
+DROP TABLE IF EXISTS "user";
+DROP TYPE IF EXISTS "gender";
