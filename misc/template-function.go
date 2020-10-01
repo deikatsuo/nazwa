c3929613688348ffca96a283a1b170a2481e924b
Deri Herdianto <derryherdianto@gmail.com>
2020-10-01 17:33:40 +0700

    Capitalize string

--- misc/template-function.go
+++ misc/template-function.go
@@ -1,6 +1,9 @@
 package misc
 
-import "html/template"
+import (
+	"html/template"
+	"strings"
+)
 
 // karena balance yang di ambil dari database
 // formatnya adalah []uint8 maka harus dirubah dulu
@@ -9,9 +12,14 @@ func balance(u []uint8) string {
 	return string(u)
 }
 
+func title(s string) string {
+	return strings.Title(s)
+}
+
 // RegTmplFunc - mendaftarkan fungsi ke template
 func RegTmplFunc() template.FuncMap {
 	return template.FuncMap{
 		"balance": balance,
+		"title":   title,
 	}
 }
