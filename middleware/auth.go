2aaacc48180c0994a586fb65f23e27e59f78a551
Deri Herdianto <derryherdianto@gmail.com>
2020-10-03 08:27:23 +0700

    Menambahkan komentar

--- middleware/auth.go
+++ middleware/auth.go
@@ -13,13 +13,17 @@ func RoutePermission(e *casbin.Enforcer) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//session := sessions.Default(c)
 		//userid := session.Get("userid")
+
+		// Role default adalah guest
 		role := "guest"
 
+		// Cek "ijin" untuk role
 		res, err := e.Enforce("role::"+role, c.Request.URL.Path, c.Request.Method)
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		// Jika tidak memiliki ijin untuk mengakses halaman
 		if !res {
 			router.Page403(c)
 			c.Abort()
