0b2ceebbaf253ebf54fc65c18ecf1b0240d82fe7
Deri Herdianto <derryherdianto@gmail.com>
2020-10-06 23:50:59 +0700

    Menambahkan alert

--- statics/js/init.js
+++ statics/js/init.js
@@ -80,17 +80,17 @@ function data() {
           break
         case 'error':
           this.alertBackgroundColor = 'bg-red-400'
-          this.alertMessage = `${this.dangerIcon} ${this.defaultDangerMessage}`
+          this.alertMessage = `${this.errorIcon} ${this.alertMessage}`
           break
       }
     },
     successIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`,
     infoIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`,
     warningIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`,
-    dangerIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path></svg>`,
+    errorIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path></svg>`,
     defaultInfoMessage: `This alert contains info message.`,
     defaultSuccessMessage: `This alert contains success message.`,
     defaultWarningMessage: `This alert contains warning message.`,
-    defaultDangerMessage: `This alert contains danger message.`,
+    defaultErrorMessage: `This alert contains danger message.`,
   }
 }
