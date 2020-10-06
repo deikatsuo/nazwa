023a71aa9cf6bdf6996ed5fddae5dfe529613e75
Deri Herdianto <derryherdianto@gmail.com>
2020-10-06 17:05:35 +0700

    Update banyak

--- router/dashboard.account.go
+++ router/dashboard.account.go
@@ -20,11 +20,16 @@ func PageDashboardAccount(c *gin.Context) {
 		"l_u_account_gender":          "Jenis kelamin",
 		"l_u_account_gender_m":        "Laki-laki",
 		"l_u_account_gender_f":        "Perempuan",
+		"l_u_account_add_phone":       "Tambah no. HP",
+		"l_u_account_phone":           "No. HP",
+		"l_u_account_add_email":       "Tambah email",
+		"l_u_account_email":           "Email",
 		"l_u_account_change_password": "Ubah kata sandi",
 		"l_u_account_password":        "Kata sandi baru",
 		"l_u_account_repassword":      "Ulangi sandi baru",
 		"l_u_account_old_password":    "Kata sandi saat ini",
-		"l_u_account_update":          "Ubah Data",
+		"l_u_account_delete_btn":      "Hapus",
+		"l_u_account_update_btn":      "Ubah Data",
 	}
 
 	df := c.MustGet("dashboard").(map[string]interface{})
---
+++ statics/js/init.js
@@ -0,0 +1,96 @@
+function data() {
+  function getThemeFromLocalStorage() {
+    // if user already changed the theme, use it
+    if (window.localStorage.getItem('dark')) {
+      return JSON.parse(window.localStorage.getItem('dark'))
+    }
+
+    // else return their preferences
+    return (
+      !!window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    )
+  }
+
+  function setThemeToLocalStorage(value) {
+    window.localStorage.setItem('dark', value)
+  }
+
+  return {
+    dark: getThemeFromLocalStorage(),
+    toggleTheme() {
+      this.dark = !this.dark
+      setThemeToLocalStorage(this.dark)
+    },
+    isSideMenuOpen: false,
+    toggleSideMenu() {
+      this.isSideMenuOpen = !this.isSideMenuOpen
+    },
+    closeSideMenu() {
+      this.isSideMenuOpen = false
+    },
+    isNotificationsMenuOpen: false,
+    toggleNotificationsMenu() {
+      this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen
+    },
+    closeNotificationsMenu() {
+      this.isNotificationsMenuOpen = false
+    },
+    isProfileMenuOpen: false,
+    toggleProfileMenu() {
+      this.isProfileMenuOpen = !this.isProfileMenuOpen
+    },
+    closeProfileMenu() {
+      this.isProfileMenuOpen = false
+    },
+    isPagesMenuOpen: false,
+    togglePagesMenu() {
+      this.isPagesMenuOpen = !this.isPagesMenuOpen
+    },
+    // Modal
+    isModalOpen: false,
+    trapCleanup: null,
+    openModal() {
+      this.isModalOpen = true
+      this.trapCleanup = focusTrap(document.querySelector('#modal'))
+    },
+    closeModal() {
+      this.isModalOpen = false
+      this.trapCleanup()
+    },
+
+    // Alert
+    openAlertBox: false,
+    alertBackgroundColor: '',
+    alertMessage: '',
+    showAlert(type) {
+      this.openAlertBox = true
+      switch (type) {
+        case 'success':
+          this.alertBackgroundColor = 'bg-green-400'
+          this.alertMessage = `${this.successIcon} ${this.defaultSuccessMessage}`
+          break
+        case 'info':
+          this.alertBackgroundColor = 'bg-blue-400'
+          this.alertMessage = `${this.infoIcon} ${this.defaultInfoMessage}`
+          break
+        case 'warning':
+          this.alertBackgroundColor = 'bg-orange-400'
+          this.alertMessage = `${this.warningIcon} ${this.defaultWarningMessage}`
+          break
+        case 'error':
+          this.alertBackgroundColor = 'bg-red-400'
+          this.alertMessage = `${this.dangerIcon} ${this.defaultDangerMessage}`
+          break
+      }
+    },
+    successIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`,
+    infoIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`,
+    warningIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>`,
+    dangerIcon: `<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 mr-2 text-white"><path d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path></svg>`,
+    defaultInfoMessage: `This alert contains info message.`,
+    defaultSuccessMessage: `This alert contains success message.`,
+    defaultWarningMessage: `This alert contains warning message.`,
+    defaultDangerMessage: `This alert contains danger message.`,
+  }
+}
