633ec0d3a2d0bd22acd508b6aec9466c6b791efc
Deri Herdianto <derryherdianto@gmail.com>
2020-10-07 23:01:49 +0700

    Menambahkan modal dan konten dinamis

--- statics/js/init.js
+++ statics/js/init.js
@@ -47,9 +47,13 @@ function data() {
     togglePagesMenu() {
       this.isPagesMenuOpen = !this.isPagesMenuOpen
     },
+
     // Modal
     isModalOpen: false,
     trapCleanup: null,
+    modalTitle: '',
+    modalContent: '',
+    modalCallback: null,
     openModal() {
       this.isModalOpen = true
       this.trapCleanup = focusTrap(document.querySelector('#modal'))
