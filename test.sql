1509762a1db7e7d5c4c05205215adc7665a339b4
Deri Herdianto <derryherdianto@gmail.com>
2020-10-29 20:28:21 +0700

    dummy

--- test.sql
+++ test.sql
@@ -43,3 +43,16 @@ JOIN "district" d ON d.id=a.district_id
 JOIN "village" v ON v.id=a.village_id
 WHERE user_id=1
 
+-- Buat peran
+INSERT INTO "product" ("name", "code","base_price","price","type","credit_twelve","credit_fifteen")
+VALUES  ('Spring bed merah jambu','SP001','1000000','1500000','spring bed','3500000','4000000'),
+        ('Liswar Jumbo','LW001','500000','1000000','liswar','2000000','2500000'),
+        ('Laptop AEX','LA001','1000000','1500000','laptop','3500000','4000000'),
+        ('Processor i3','PR001','1000000','1500000','processor','3500000','4000000'),
+        ('Motor honda x','MO001','12000000','15000000','motor','35000000','50000000'),
+        ('Mobil chevrolet s','MB001','1000000','1500000','mobil','3500000','4000000'),
+        ('Kapal ferry Z','KP001','1000000','1500000','kapal','3500000','4000000'),
+        ('Jet Pack Radio 2','JT001','1000000','1500000','pesawat','3500000','4000000'),
+        ('Pesawat jet hover','JT002','1000000','1500000','pesawat','3500000','4000000'),
+        ('Rumah box solo','RM001','1000000','1500000','rumah','3500000','4000000');
+
