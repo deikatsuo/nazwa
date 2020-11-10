ecae4d5d3f581ee9e5ac5a3d7a6db415f0624259
Deri Herdianto <derryherdianto@gmail.com>
2020-11-10 20:12:06 +0700

    Menambahkan baris komentar

--- dbquery/product.go
+++ dbquery/product.go
@@ -45,13 +45,16 @@ func (p *GetProducts) Show(db *sqlx.DB) ([]wrapper.Product, error) {
 		limit = p.limit
 	}
 
+	// Where logic
 	where := ""
-	if p.direction == "next" {
+	// Maju/Mundur
+	if p.direction == "next" && p.lastid > 0 {
 		where = "WHERE id > " + strconv.Itoa(p.lastid)
 	} else if p.direction == "back" {
-
+		where = "WHERE id < " + strconv.Itoa(p.lastid) + " ORDER BY id DESC"
 	}
 
+	// query pengambilan data produk
 	query := `SELECT
 		id,
 		name,
@@ -72,41 +75,7 @@ func (p *GetProducts) Show(db *sqlx.DB) ([]wrapper.Product, error) {
 		return []wrapper.Product{}, err
 	}
 
-	for _, p := range product {
-		parse = append(parse, wrapper.Product{
-			ID:        p.ID,
-			Name:      strings.Title(p.Name),
-			CreatedAt: p.CreatedAt,
-			BasePrice: string(p.BasePrice),
-			Price:     string(p.Price),
-			Code:      string(p.Code.String),
-		})
-	}
-
-	return parse, nil
-}
-
-// GetAllProduct menampilkan semua product
-func GetAllProduct(db *sqlx.DB) ([]wrapper.Product, error) {
-	var product []wrapper.NullableProduct
-	var parse []wrapper.Product
-
-	query := `SELECT
-		id,
-		name,
-		TO_CHAR(base_price,'Rp999G999G999G999G999') AS base_price,
-		TO_CHAR(price,'Rp999G999G999G999G999') AS price,
-		code,
-		TO_CHAR(created_at, 'MM/DD/YYYY HH12:MI:SS AM') AS created_at
-		FROM "product"`
-
-	err := db.Select(&product, query)
-	if err != nil {
-		log.Println("product.go Select all product")
-		log.Println(err)
-		return []wrapper.Product{}, err
-	}
-
+	// mapping data produk
 	for _, p := range product {
 		parse = append(parse, wrapper.Product{
 			ID:        p.ID,
