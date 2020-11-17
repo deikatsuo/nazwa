fa85cb279f728440d9d44a562a16e428c9a4233b
Deri Herdianto <derryherdianto@gmail.com>
2020-11-17 10:13:15 +0700

    testql

--- test.sql
+++ test.sql
@@ -45,17 +45,18 @@ JOIN "village" v ON v.id=a.village_id
 WHERE user_id=1
 
 -- Buat peran
-INSERT INTO "product" ("name", "code","base_price","price","type","credit_twelve","credit_fifteen")
-VALUES  ('Spring bed biru jambu','SP002','1000000','1500000','spring bed','3500000','4000000'),
-        ('Liswar Jumbo2x','LW002','500000','1000000','liswar','2000000','2500000'),
-        ('Laptop Dell','LA002','1000000','1500000','laptop','3500000','4000000'),
-        ('Processor i5','PR002','1000000','1500000','processor','3500000','4000000'),
-        ('Motor yamaha','MO002','12000000','15000000','motor','35000000','50000000'),
-        ('Mobil ford','MB002','1000000','1500000','mobil','3500000','4000000'),
-        ('Kapal cargo','KP002','1000000','1500000','kapal','3500000','4000000'),
-        ('Jet Pack Laser','JT003','1000000','1500000','pesawat','3500000','4000000'),
-        ('Pesawat jet slam','JT004','1000000','1500000','pesawat','3500000','4000000'),
-        ('Rumah lipat','RM002','1000000','1500000','rumah','3500000','4000000');
+INSERT INTO "product" ("name", "code","base_price","price","type","brand")
+VALUES  ('Spring bed biru jambu','SP002','1000000','1500000','spring bed','samsung'),
+        ('Liswar Jumbo2x','LW002','500000','1000000','liswar','axis'),
+        ('Laptop Dell','LA002','1000000','1500000','laptop','nokia'),
+        ('Processor i5','PR002','1000000','1500000','processor','xiaomi'),
+        ('Motor yamaha','MO002','12000000','15000000','motor','honda'),
+        ('Mobil ford','MB002','1000000','1500000','mobil','yamaha'),
+        ('Kapal cargo','KP002','1000000','1500000','kapal','burik'),
+        ('Jet Pack Laser','JT003','1000000','1500000','pesawat','kucuk'),
+        ('Pesawat jet slam','JT004','1000000','1500000','pesawat','apple'),
+        ('Pesawat jet tempur','JT005','1000000','1500000','pesawat','huawei'),
+        ('Rumah lipat','RM002','1000000','1500000','rumah','acer');
 
 SELECT
 		u.id,
