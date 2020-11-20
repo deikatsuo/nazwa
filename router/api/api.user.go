d97d564dfe702571b680bad0a0b663a5ceb1829b
Deri Herdianto <derryherdianto@gmail.com>
2020-11-20 10:33:07 +0700

    Update

--- router/api/api.user.go
+++ router/api/api.user.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"io/ioutil"
+	"log"
 	"nazwa/dbquery"
 	"nazwa/misc"
 	"nazwa/misc/validation"
@@ -16,16 +17,96 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+////////////
+// CREATE //
+////////////
+
+// FormUser menyimpan input pendaftaran user
+type FormUser struct {
+	FC         string `json:"fc" binding:"omitempty,numeric,min=16,max=16"`
+	RIC        string `json:"ric" binding:"numeric,min=16,max=16"`
+	Phone      string `json:"phone" binding:"omitempty,numeric,min=6,max=15"`
+	Firstname  string `json:"firstname" binding:"required,min=3,max=25"`
+	Lastname   string `json:"lastname" binding:"omitempty,min=1,max=25"`
+	Gender     string `json:"gender" binding:"required,oneof=m f"`
+	Occupation string `json:"occupation" binding:"omitempty,min=4,max=25"`
+	Password   string `json:"password" binding:"omitempty,alphanumunicode,min=8,max=25"`
+	Repassword string `json:"repassword" binding:"eqfield=Password"`
+}
+
+// UserCreate API untuk membuat user baru
+func UserCreate(db *sqlx.DB) gin.HandlerFunc {
+	fn := func(c *gin.Context) {
+		var json FormUser
+
+		var status string
+		var httpStatus int
+		message := ""
+		var simpleErrMap = make(map[string]interface{})
+		save := true
+
+		if err := c.ShouldBindJSON(&json); err != nil {
+			log.Println("ERROR: api.create-account.go UserCreate() bind json")
+			log.Println(err)
+			simpleErrMap = validation.SimpleValErrMap(err)
+			httpStatus = http.StatusBadRequest
+			status = "fail"
+			save = false
+		} else {
+			if dbquery.RICExist(db, json.RIC) {
+				simpleErrMap["ric"] = "Nomor KTP sudah terdaftar"
+				save = false
+			}
+			if dbquery.PhoneExist(db, json.Phone) {
+				simpleErrMap["phone"] = "Nomor ini sudah terdaftar"
+				save = false
+			}
+		}
+
+		if save {
+			user := dbquery.NewUser()
+			err := user.SetFirstName(json.Firstname).
+				SetLastName(json.Lastname).
+				SetFamilyCard(json.FC).
+				SetRIC(json.RIC).
+				SetPhone(json.Phone).
+				SetPassword(json.Password).
+				SetGender(json.Gender).
+				SetOccupation(json.Occupation).
+				SetRole(dbquery.RoleCustomer).
+				Save(db)
+			if err != nil {
+				log.Println("ERROR: api.create-account.go UserCreate() Gagal membuat user baru")
+				log.Print(err)
+			}
+			httpStatus = http.StatusOK
+			status = "success"
+			message = "Berhasil membuat user baru"
+		} else {
+			httpStatus = http.StatusBadRequest
+			status = "error"
+			message = "Gagal membuat pelanggan, silahkan perbaiki formulir"
+		}
+
+		m := gin.H{
+			"message": message,
+			"status":  status,
+		}
+		c.JSON(httpStatus, misc.Mete(m, simpleErrMap))
+	}
+	return gin.HandlerFunc(fn)
+}
+
+////////////
+/* UPDATE */
+////////////
+
 type updateContact struct {
 	Username    string `json:"username" binding:"alphanum,min=4,max=25"`
 	Password    string `json:"password" binding:"omitempty,alphanumunicode,min=8,max=25"`
 	Repassword  string `json:"repassword" binding:"eqfield=Password"`
 	Oldpassword string `json:"oldpassword" binding:"required_with=Password"`
 }
-
-////////////
-/* UPDATE */
-////////////
 
 // UserUpdateContact api untuk mengupdate kontak user
 func UserUpdateContact(db *sqlx.DB) gin.HandlerFunc {
