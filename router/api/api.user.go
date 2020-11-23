0480425cfe0a791fc2ac0b85e2b1dc64cda1fef2
Deri Herdianto <derryherdianto@gmail.com>
2020-11-23 18:57:14 +0700

    Update

--- router/api/api.user.go
+++ router/api/api.user.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"nazwa/dbquery"
 	"nazwa/misc"
 	"nazwa/misc/validation"
@@ -25,16 +24,17 @@ import (
 
 // FormUser menyimpan input pendaftaran user
 type FormUser struct {
-	FC         string         `json:"fc" binding:"omitempty,numeric,min=16,max=16"`
-	RIC        string         `json:"ric" binding:"numeric,min=16,max=16"`
-	Phone      string         `json:"phone" binding:"omitempty,numeric,min=6,max=15"`
-	Firstname  string         `json:"firstname" binding:"required,min=3,max=25"`
-	Lastname   string         `json:"lastname" binding:"omitempty,min=1,max=25"`
-	Gender     string         `json:"gender" binding:"required,oneof=m f"`
-	Occupation string         `json:"occupation" binding:"omitempty,min=4,max=25"`
-	Password   string         `json:"password" binding:"omitempty,alphanumunicode,min=8,max=25"`
-	Repassword string         `json:"repassword" binding:"eqfield=Password"`
-	Photo      multipart.File `json:"photo"`
+	FC         string `json:"fc" binding:"omitempty,numeric,min=16,max=16"`
+	RIC        string `json:"ric" binding:"numeric,min=16,max=16"`
+	Phone      string `json:"phone" binding:"omitempty,numeric,min=6,max=15"`
+	Firstname  string `json:"firstname" binding:"required,min=3,max=25"`
+	Lastname   string `json:"lastname" binding:"omitempty,min=1,max=25"`
+	Gender     string `json:"gender" binding:"required,oneof=m f"`
+	Occupation string `json:"occupation" binding:"omitempty,min=4,max=25"`
+	Password   string `json:"password" binding:"omitempty,alphanumunicode,min=8,max=25"`
+	Repassword string `json:"repassword" binding:"eqfield=Password"`
+	Photo      string `json:"photo" binding:"omitempty,base64"`
+	PhotoType  string `json:"photo_type"`
 }
 
 // UserCreate API untuk membuat user baru
