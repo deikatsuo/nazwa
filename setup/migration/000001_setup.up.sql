0509231197940b6637d2047960bd42a633d5a4a0
Deri Herdianto <derryherdianto@gmail.com>
2021-06-07 12:13:53 +0700

    comment insert

--- setup/migration/000001_setup.up.sql
+++ setup/migration/000001_setup.up.sql
@@ -51,8 +51,8 @@ CREATE TABLE "user" (
 );
 
 -- Buat user
-INSERT INTO "user" ("first_name", "username", "password", "gender")
-    VALUES ('deri', 'deri', 'tetehrika', 'm');
+-- INSERT INTO "user" ("first_name", "username", "password", "gender")
+--     VALUES ('deri', 'deri', 'tetehrika', 'm');
 
 
 -- Tabel KK
@@ -83,8 +83,8 @@ CREATE TABLE "user_role" (
 );
 
 -- Buat user
-INSERT INTO "user_role" ("role_id", "user_id")
-    VALUES (1, 1);
+-- INSERT INTO "user_role" ("role_id", "user_id")
+--     VALUES (1, 1);
 
 
 -- Tabel email
